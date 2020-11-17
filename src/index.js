26fd237b2fcec6340344ccdcaca751a5ba1a8406
luismarcelino <luismarcelino@gmail.com>
2020-11-17 22:29:48 +0000

    GPIO test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+const { Gpio } = require( 'onoff' );
+ 
+const ledOut = new Gpio( '17', 'out' );
+let isLedOn = false;
+ 
+// run a infinite interval
+setInterval( () => {
+ ledOut.writeSync( isLedOn ? 0 : 1 ); // provide 1 or 0
+ isLedOn = !isLedOn; // toggle state
+}, 3000 ); // 3s
+
+
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Sweet Home!')
 })
